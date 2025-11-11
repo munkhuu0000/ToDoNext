e5de677ddd5b22a03ebe5d749c518bc7bfc04c2a
25LP8703 25LP8703 <25LP8703@pinecones-iMac.local>
2025-11-11 11:55:50 +0800

    ad3

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use Client";
+"use client";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -9,19 +9,47 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { InputGroupButton } from "@/components/ui/input-group";
 import Image from "next/image";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+const tabs = ["All", "Completed", "Incomplete"];
 
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
+  console.log(todos);
+
   return (
     <div className="flex justify-center pt-10">
       <Card className="w-[400px] h-[fit] flex justify-center ">
         <CardTitle className="flex justify-center">To Do List</CardTitle>
         <CardContent className="flex flex-row gap-1.5">
-          <Input className="w-[280px] h-10" placeholder="Add a new task ..." />
-          <Button className="w-15 h-10 bg-[#3C82F6] hover:bg-[#3C82F6]">
-            {" "}
+          <Input
+            className="w-[280px] h-10"
+            placeholder="Add a new task ..."
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
+          <Button
+            className="w-15 h-10 bg-[#3C82F6] hover:bg-[#3C82F6]"
+            onClick={() => {
+              setTodos([
+                ...todos,
+                {
+                  id: nanoid(),
+                  isDone: false,
+                  text: value,
+                },
+              ]);
+              setValue("");
+            }}
+          >
             Add
           </Button>
         </CardContent>
@@ -36,11 +64,39 @@ export default function Home() {
             Completed
           </Button>
         </CardContent>
+        <div className="flex flex-col gap-4 px-6 ">
+          {todos.map((item) => (
+            <div key={item.id}>
+              <CardContent className="flex gap-4 justify-between p-0">
+                <Checkbox
+                  onClick={() => {
+                    const newTodos = todos.map((todo) => {
+                      if (todo.id !== item.id) return todo;
+                      return {
+                        isDone: !item.isDone,
+                        text: item.text,
+                        id: item.id,
+                      };
+                    });
+                    setTodos(newTodos);
+                  }}
+                />
+                <p className="flex-1">{item.text}</p>
+                <Button className="bg-[#FEF2F2] text-[#EF4444] font-normal text-sm">
+                  Delete
+                </Button>
+              </CardContent>
+            </div>
+          ))}
+        </div>
         <CardContent className="flex justify-center">
           <p className="text-[#6B7280]">No tasks yet. Add one above!</p>
         </CardContent>
-        <CardContent className="flex justify-center">
-          <p className="text-[#6B7280]">Powered by Pinecone academy.</p>
+        <CardContent className="flex justify-center gap-1.5">
+          <p className="text-[#6B7280]">Powered by </p>
+          <a href="https://pinecone.mn/">
+            <p className="text-[#3B73ED]">Pinecone academy.</p>
+          </a>
         </CardContent>
       </Card>
     </div>
